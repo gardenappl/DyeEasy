24553f553d5d6b3d6cd2b43c52e17969128de029
Pop000100 <Pop000100@outlook.com>
2023-07-28 08:08:55 -0800

    Update to 1.4.4 (#3)
    * Update to 1.4.4
    
    - Move the recipe code to ModSystem since Mod.AddRecipes has been deprecated.

--- DyeEasy.cs
+++ DyeEasy.cs
@@ -9,60 +9,60 @@ namespace DyeEasy
 {
 	public class DyeEasy : Mod
 	{
-		List<int> changedRecipes = new List<int>();
-		List<int> originalStack = new List<int>();
-		
+		//Hamstar's Mod Helpers integration
+		public static string GithubUserName { get { return "goldenapple3"; } }
+		public static string GithubProjectName { get { return "DyeEasy"; } }
+	}
+
+	public class DyeEasyRecipes : ModSystem
+	{
+		Dictionary<int, int> changedRecipes = new Dictionary<int, int>();
+
 		public override void AddRecipes()
 		{
-			for(int i = 0; i < Main.recipe.Length; i++)
+			for (int i = 0; i < Main.recipe.Length; i++)
 			{
 				var recipe = Main.recipe[i];
-				
-				if(recipe.createItem.dye != 0)
+
+				if (recipe.createItem.dye != 0)
 				{
 					int dyeIngredients = 0;
 					var foundDyeTypes = new List<int>();
 					bool foundBottledWater = false;
-					
-					foreach(var item in recipe.requiredItem)
+
+					foreach (var item in recipe.requiredItem)
 					{
-						if(item != null)
+						if (item != null)
 						{
-							if(item.dye != 0)
+							if (item.dye != 0)
 							{
 								dyeIngredients += item.stack;
 								foundDyeTypes.Add(item.type);
 							}
-							else if(item.type == ItemID.BottledWater)
+							else if (item.type == ItemID.BottledWater)
 								foundBottledWater = true;
 						}
 					}
+					int originalStack = recipe.createItem.stack;
 
-					originalStack.Add(recipe.createItem.stack);
 					//Lunar dyes (ignore)
 					if (foundBottledWater)
 						continue;
 					//Basic dyes
-					if(dyeIngredients == 0)
+					if (dyeIngredients == 0)
 						recipe.createItem.stack = 3;
 					//Compound dyes
-					else if(foundDyeTypes.Count > 1)
+					else if (foundDyeTypes.Count > 1)
 						recipe.createItem.stack = dyeIngredients;
-					
-					changedRecipes.Add(i);
+					changedRecipes.Add(i, originalStack);
 				}
 			}
 		}
-		
-		public override void Unload() //Reverting changes
+		public override void Unload()
 		{
-			foreach(int recipeIndex in changedRecipes)
-				if(recipeIndex < Main.recipe.Length) //You never know
-					Main.recipe[recipeIndex].createItem.stack = originalStack[recipeIndex];
+			foreach (var kvp in changedRecipes)
+				if (kvp.Key < Main.recipe.Length)
+					Main.recipe[kvp.Key].createItem.stack = kvp.Value;
 		}
-
-		//Hamstar's Mod Helpers integration
-		public static string GithubUserName { get { return "goldenapple3"; } }
-		public static string GithubProjectName { get { return "DyeEasy"; } }
 	}
 }
